3853742530c968e8603558e4f1b65c00d6b72347
redfox <8564093@qq.com>
2026-05-19 23:44:37 +0800

    refactor(protocol): move command constants to separate file
    将原protocol.go中的命令常量抽离到单独的command.go文件中，统一管理协议命令码，提升代码可维护性

---
+++ server/internal/protocol/command.go
@@ -0,0 +1,9 @@
+package protocol
+
+const (
+	CmdCalculate uint32 = 1001
+	CmdSmall     uint32 = 2001
+	CmdMedium    uint32 = 3001
+	CmdLarge     uint32 = 4001
+	CmdShutDown  uint32 = 9999
+)
--- server/internal/protocol/protocol.go
+++ server/internal/protocol/protocol.go
@@ -10,19 +10,7 @@ import (
 
 const (
 	HeaderLen = 8
-
-	CmdCalculate uint32 = 1001
-	CmdSmall     uint32 = 2001
-	CmdMedium    uint32 = 3001
-	CmdLarge     uint32 = 4001
-	CmdShutDown  uint32 = 9999
 )
-
-// Conn 定义连接接口
-// type Conn interface {
-// 	RemoteAddr() net.Addr
-// 	AsyncWrite([]byte, gnet.AsyncCallback) error
-// }
 
 func SendPacket(c gnet.Conn, cmdID uint32, body []byte) {
 	var header [HeaderLen]byte
